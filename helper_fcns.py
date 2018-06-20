dcd7d24f2f450d3b1dc7676bfd9d045c3ca45082
paul-levy <paul.levy@nyu.edu>
2018-06-20 12:41:53 -0400

    updates for S2 analysis

--- helper_fcns.py
+++ helper_fcns.py
@@ -11,11 +11,24 @@ def geo_mean(nparr):
     n = len(nparr);
     return np.power(np.prod(nparr), 1.0/n);
 
-def find_pse(true_pmf, params):
+def find_pse_help(true_pmf, params, init_x = 0.5):
     # 0.5 is PSE; i.e., we believe the test and reference SF are equivalent
     pse_obj = lambda x: np.square(0.5 - true_pmf(params, x));
-    pse_opt = opt.minimize(pse_obj, 0.5);
+    pse_opt = opt.minimize(pse_obj, init_x);
     return pse_opt['x'], pse_opt;
+
+def find_pse(true_pmf, params):
+    min_pse = []; min_opt = [];
+    for i in np.arange(0, 1, 0.01):
+      curr_pse, curr_opt = find_pse_help(true_pmf, params, i);
+      if min_pse: # i.e. not empty
+        if curr_opt['fun'] < min_opt['fun']:
+          min_pse = curr_pse;
+          min_opt = curr_opt;
+      else:
+        min_pse = curr_pse;
+        min_opt = curr_opt;
+    return min_pse, min_opt;
 
 def fit_pmf_logit(obs, sfs):
     '''
